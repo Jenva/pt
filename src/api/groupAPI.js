49ff53ac8f84f636b621f87ddf9905661319d10d
jenva <jenva810@163.com>
2021-06-29 17:35:49 +0800

    fix

---
+++ src/api/groupAPI.js
@@ -0,0 +1,34 @@
+import APIHelper from './APIHelper'
+
+export default {
+  getGroupList (params) {
+    const url = '/group/list'
+    return APIHelper.createAxios().get(
+      url, { params }
+    )
+  },
+  addGroup (params) {
+    const url = '/group/save'
+    return APIHelper.createAxios().post(
+      url, params
+    )
+  },
+  deleteGroup (id) {
+    const url = `/group/delete/${id}`
+    return APIHelper.createAxios().delete(
+      url
+    )
+  },
+  cameraList (params) {
+    const url = '/camera-areas/listByGroupId'
+    return APIHelper.createAxios().get(
+      url, { params }
+    )
+  },
+  saveCamera (params) {
+    const url = '/camera-areas/save'
+    return APIHelper.createAxios().post(
+      url, params 
+    )
+  }
+}
